7581b1a99d104ee48d59d9ab99304fc055a20b3c
Rei-Barjami <61749909+Rei-Barjami@users.noreply.github.com>
2025-06-12 18:29:16 +0200

    Update errorInj.py

--- errorInj.py
+++ errorInj.py
@@ -8,11 +8,8 @@ from tensorflow.keras.models import Model
 
 def flip_float_bits(input_float, bit_position):
     float_bits = tf.bitcast(input_float, tf.uint32)
-    # Create a bitmask with a 1 at the specified bit_position
     bitmask = 1 << bit_position
-    # Use bitwise XOR to flip the bit
     modified_float_bits = tf.bitwise.bitwise_xor(float_bits, bitmask)
-    # Convert the modified bits back to a float
     modified_float = tf.bitcast(modified_float_bits, tf.float32)
     return modified_float
 
@@ -24,12 +21,9 @@ class BinaryBitFlipNoiseLayer(tf.keras.layers.Layer):
         self.bit_flip_probability = bit_flip_probability
 
     def call(self, inputs, training=None):
-            # Generate random noise
             noise = tf.random.uniform(tf.shape(inputs), dtype=tf.float32)
-            # Determine which bits to flip
             flip_bits = noise < self.bit_flip_probability
 
-            # Apply bit-flip noise to each element in the tensor
             noisy_output = tf.identity(inputs)
             for bit_position in range(32):
                 flip_condition = flip_bits
